91cb253c93df52018b172ac1aa435b175e4811c7
ibdrok <ibsonsilva54@gmail.com>
2026-04-23 19:32:45 -0300

    Create apps.sh
    Install necessary packages and add Flathub repository for Flatpak.

---
+++ apps.sh
@@ -0,0 +1,8 @@
+sudo apt update && apt upgrade 
+apt install steam-installer
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo apt install flatpak
+sudo apt install plasma-discover-backend-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+wget https://github.com/balena-io/etcher/releases/download/v2.1.4/balenaEtcher-linux-x64-2.1.4.zip
